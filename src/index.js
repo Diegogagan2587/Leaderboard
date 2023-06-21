27a4513922fb7af8d135bab87163e62e71f09239
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-20 18:56:45 -0600

    Add some scores using javascript

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './style.css';
 
+const scoresData = [
+    {name:'name', score: 100},
+    {name:'name', score: 1},
+    {name:'name', score: 4},
+    {name:'name', score: 5},
+    {name:'name', score: 4}
+]
+
+const scoresContainer = document.getElementById('scores-list');
+
+scoresData.forEach(score => {
+    const newScore = document.createElement('li');
+    newScore.innerHTML = `<span>${score.name}:</span>${score.score}`;
+    scoresContainer.appendChild(newScore);
+});
