165a5c477ea5853e471c2a3dd6f3dd6282f1fd19
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-22 10:13:01 -0700

    refactor to use await instead of .then

--- src/modules/create_new_game.js
+++ src/modules/create_new_game.js
@@ -1,9 +1,9 @@
-
 const getGameID = async (object) => {
   const arrFromObj = object.result.split(' ');
   const result = arrFromObj[3];
+
   return result;
-}
+};
 
 async function createNewGame() {
   const urlAPI = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
@@ -16,14 +16,15 @@ async function createNewGame() {
     body: JSON.stringify({
       name: 'Diego Vidal Game',
     }),
-  })
-    .then((response) => response.json()); // then get the ID of the new game
+  });
 
-  const gameID = getGameID(newGame);
+  const response = await newGame.json();
+
+  const gameID = getGameID(response);
   // return the Id of the new Object
   return gameID;
 }
 
-const testGameID = 'kW68PZGsF4CTjYa0TPRr';
+const testGameID = 'zCc9R3dmsr9pYokfcmgO';
 
 export { createNewGame, testGameID };
