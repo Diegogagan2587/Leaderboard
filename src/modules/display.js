b6d651b9b017a6407e2a89bf3a9d25e2043e4669
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-06-21 21:38:11 -0600

    rename varialbes to improve readability

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,8 +1,9 @@
 
-const display = (scores) => {
+const display = (scoreList) => {
     const scoresContainer = document.getElementById('scores-list');
+    scoresContainer.innerHTML = '';
 
-    scores.forEach((score) => {
+    scoreList.forEach((score) => {
         const newScore = document.createElement('li');
         newScore.innerHTML = `<span>${score.name}:</span>${score.score}`;
         scoresContainer.appendChild(newScore);
